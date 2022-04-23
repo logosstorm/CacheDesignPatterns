dab2a8f368a8ba08a0c3be5ca6e4ac5199bc1ab9
fatih çakıroğlu <f-cakiroglu@outlook.com>
2022-04-23 18:48:50 +0300

    initial

---
+++ CacheApp.Cache/RedisService.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+namespace CacheApp.Cache;
+
+public class RedisService
+{
+    private ConnectionMultiplexer _connectionMultiplexer;
+    public IDatabase Database = null!;
+
+    public RedisService() => _connectionMultiplexer = ConnectionMultiplexer.Connect("localhost");
+
+    public IDatabase GetDb(int db) => _connectionMultiplexer.GetDatabase(db);
+
+}
